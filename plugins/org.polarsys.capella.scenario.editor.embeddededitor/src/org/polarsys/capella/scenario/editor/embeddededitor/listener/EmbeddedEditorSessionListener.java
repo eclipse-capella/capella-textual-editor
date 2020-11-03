3dc6124af085570eb000fcb0e0a4e701ccae8a20
Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>
2020-11-03 09:30:15 +0200

    Fix build missing dependencies
    - fix close editor when selecting a view part
    
    
    Change-Id: I681eb6738164878968b572952fc39e324faf1d57
    Signed-off-by: Georgiana-Elena ECOBICI <georgiana-elena.ecobici@thalesgroup.com>

--- plugins/org.polarsys.capella.scenario.editor.embeddededitor/src/org/polarsys/capella/scenario/editor/embeddededitor/listener/EmbeddedEditorSessionListener.java
+++ plugins/org.polarsys.capella.scenario.editor.embeddededitor/src/org/polarsys/capella/scenario/editor/embeddededitor/listener/EmbeddedEditorSessionListener.java
@@ -25,6 +25,7 @@ import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.navigator.CommonNavigator;
+import org.eclipse.ui.part.ViewPart;
 import org.polarsys.capella.core.data.interaction.Scenario;
 import org.polarsys.capella.core.model.handler.helpers.CapellaAdapterHelper;
 import org.polarsys.capella.scenario.editor.EmbeddedEditorInstance;
@@ -107,8 +108,8 @@ public class EmbeddedEditorSessionListener implements SessionManagerListener {
             activePage.hideView(eeView);
           }
         }
-      } else if (!(part instanceof CommonNavigator) && eeView != null && activePage != null) {
-        activePage.hideView(eeView);
+      } else if ((!(part instanceof CommonNavigator) && !(part instanceof ViewPart)) && eeView != null && activePage != null) {
+	        activePage.hideView(eeView);
       }
     };
   }
